96c818b5ddb2d8277ca5028a2018441740453e34
akhilmondal <akhilmandalfamily@gmail.com>
2023-07-18 10:33:11 +0530

    [Akhil] Added: API integration is done for login user.

--- src/app/Services/httpServices/http-services.service.ts
+++ src/app/Services/httpServices/http-services.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpServicesService {
-  baseurl = 'localhost:3000/api/v1/';
+  baseurl = 'http://localhost:3000/api/v1/';
 
-
-  constructor(private httpClient: HttpClient) { }
-  postService(url: string) {
-
+  constructor(private httpClient: HttpClient) {}
+  postService(
+    url: string,
+    reqdata: any,
+    token: boolean = false,
+    httpOptions: any
+  ) {
+    return this.httpClient.post(
+      this.baseurl + url,
+      reqdata,
+      token && httpOptions
+    );
   }
 }
-
